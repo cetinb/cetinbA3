4cdb1c1fcbf7e10bdeef31346a1e7627147ed343
cetinb <52579148+cetinb@users.noreply.github.com>
2019-07-25 23:41:36 -0400

    Add files via upload

---
+++ assets/data/myBook.ts
@@ -0,0 +1,31 @@
+import {book} from '../../app/bookInfo';
+
+
+export const MYBOOKS: book[] =[
+    {bookname: 'Harry Potter 1',
+    bookauthor: 'J.K Rowling',
+    bookGenre:'Fantasy',
+    bookPublish: 'Nov 3, 2000',
+    picture: 'hp1.jpg'
+    },
+    {bookname: 'Harry Potter 2',
+    bookauthor: 'J.K Rowling',
+    bookGenre:'Fantasy',
+    bookPublish: 'Sep 10, 2002',
+    picture: 'hp2.jpg'
+    },
+    {bookname: 'Harry Potter 3',
+    bookauthor: 'J.K Rowling',
+    bookGenre:'Fantasy',
+    bookPublish: 'August 2, 2005',
+    picture: 'hp3.jpg'
+    },
+    {bookname: 'Harry Potter 4',
+    bookauthor: 'J.K Rowling',
+    bookGenre:'Fantasy',
+    bookPublish: 'Nov 20, 2006',
+    picture: 'hp4.jpg'
+    }
+
+]
+   
